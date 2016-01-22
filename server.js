93476d77494bc93398360128556c62d97edd3485
Roger Hutchings <roger@hutchin.gs>
2016-01-22 10:31:50 +0000

    Add morgan

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express');
+var morgan = require('morgan');
 
 // Constants
 var PORT = 8080;
 
 // App
 var app = express();
+
+app.use(morgan('dev'));
 
 app.use('/', express.static('/app/build'));
 
